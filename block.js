a1874f8ac8a32dcae8fa264cf0955f6c61229437
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-05-18 01:35:52 +0200

    initial commit

---
+++ block.js
@@ -0,0 +1,19 @@
+class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  toString() {
+    return `Block - 
+      Timestamp: ${this.timestamp}
+      LastHash : ${this.lastHash}
+      Hash     : ${this.hash}
+      Data     : ${this.data}
+    `
+  }
+}
+
+module.exports = Block;
