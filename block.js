4720cd539812218250d45185c82879c9b484e52a
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-05-18 13:37:22 +0200

    Add mine block function to be able to mine each block

--- block.js
+++ block.js
@@ -1,3 +1,4 @@
+const SHA256 = require('crypto-js/sha256');
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp = timestamp;
@@ -9,10 +10,26 @@ class Block {
   toString() {
     return `Block - 
       Timestamp: ${this.timestamp}
-      LastHash : ${this.lastHash}
-      Hash     : ${this.hash}
+      LastHash : ${this.lastHash.substring(0, 10)}
+      Hash     : ${this.hash.substring(0, 10)}
       Data     : ${this.data}
     `
+  }
+
+  static genesis() {
+    return new this('Genesis time', '-----', 'f1r57-h45h', []);
+  }
+
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 }
 
