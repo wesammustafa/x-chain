263f4b8699eca9121cd144dee39935b612100c4d
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-06-22 21:36:49 +0200

    Initialize the infrastructure of p2p server

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -14,20 +14,29 @@ class P2pServer {
     const server = new WebSocket.Server({ port: P2P_PORT });
     server.on('connection', socket => this.connectSocket(socket));
     this.connectToPeers();
-    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`)    
+    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`)
   }
 
   connectToPeers() {
-    peers.forEach( peer => {
+    peers.forEach(peer => {
       const socket = new WebSocket(peer);
 
       socket.on('open', () => this.connectSocket(socket));
     })
   }
-  
+
   connectSocket(socket) {
     this.sockets.push(socket);
-    console.log('Socket connected')
+    console.log('Socket connected');
+    this.messageHandler(socket);
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+    })
   }
 }
 
