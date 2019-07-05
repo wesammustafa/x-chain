7d0dcc6c308425fde7fed5e5de4e9546fcb6b3c6
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-07-05 19:31:30 +0200

    Create Chain utils class in order to put all uitility methods together

--- wallet/index.js
+++ wallet/index.js
@@ -1,9 +1,10 @@
 const { INITIAL_BALANCE } = require('../config.js');
+const ChainUtil = require('../chain-util');
 class Wallet {
   constructor() {
     this.balance = INITIAL_BALANCE;
-    this.keyPair = null;
-    this.publicKey = null;
+    this.keyPair = ChainUtil.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
   }
 
   toString() {
