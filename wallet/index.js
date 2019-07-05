dfc7bf9dc5ff0f2c068e40633da23a421afc4b65
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-07-05 18:32:28 +0200

    Initialize wallet Class

---
+++ wallet/index.js
@@ -0,0 +1,16 @@
+const { INITIAL_BALANCE } = require('../config.js');
+class Wallet {
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = null;
+    this.publicKey = null;
+  }
+
+  toString() {
+    return `Wallet - 
+      publicKey: ${this.publicKey.toString()}
+      balance  : ${this.balance}`
+  }
+}
+
+module.exports = Wallet;
